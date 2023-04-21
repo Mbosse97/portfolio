14bd27b242cb472ab1c1edd3c049b31bcc9aa135
Michael B <bosse730@gmail.com>
2023-04-22 01:52:39 +1000

    Small list of skills added

---
+++ portfolio-app/src/components/Resume.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Resume() {
+    return (
+        <div> 
+            <p> You can download my resume here</p>
+            <h3>Here are some of my skills</h3>
+            <ul> 
+                <li>React</li>
+                <li>MongoDb</li>
+                <li>GraphQL</li>
+                <li>PWA's</li>
+            </ul>
+        </div>
+    );
+}
