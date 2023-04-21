5eec5829c37bdfe3b9b260ec35c9987aa23971cb
Michael B <bosse730@gmail.com>
2023-04-22 01:51:34 +1000

    About me section added

---
+++ portfolio-app/src/components/Aboutme.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const styles = {
+    header: {
+
+    },
+    text: {
+
+    }
+};
+
+export default function AboutMe() {
+    return (
+        <div>
+            <h2> 
+                About Me 
+            </h2>
+            <p>
+                My name is Michael Bosse and I am a Wed Development Student, currently undertaking a Coding Bootcamp
+                with Sydney University. 
+                I am currenly a Civil Engineer, however I am looking to make a move into Web Development and thought
+                this course would be helpful for me to achieve this goal. 
+                Please take some time to look through my portfolio of projects and download my resume. 
+            </p>
+        </div>
+    );
+}
+
