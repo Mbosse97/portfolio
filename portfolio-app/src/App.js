6bfb296f36710ffd47eae8f35be5b3e48a834e3a
Michael B <bosse730@gmail.com>
2023-04-22 01:51:12 +1000

    added navigation to pages and props

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import { useState } from 'react';
+import AboutMe from './components/Aboutme';
+import Contact from './components/Contact';
+import Navigation from './components/Navigation';
+import Portfolio from './components/Portfolio';
+import Project from './components/Project';
+import Resume from './components/Resume'
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header handlePageChange={handlePageChange}/>
+        {currentPage === 'AboutMe' && <AboutMe />}
+        {currentPage === 'Portfolio' && <Portfolio />}
+        {currentPage === 'Contact' && <Contact />}
+        {currentPage === 'Resume' && <Resume />}
+        <Footer />
     </div>
   );
 }
